2b382020e37cdc13bbf671876a15f3d35fa427e1
rosh <rosherna@Roshernas-MacBook-Air.local>
2022-02-22 19:25:04 +0800

    added unique validator to user schema

--- models/user.js
+++ models/user.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
     email: {type: String, required: true, unique: true},
-    password: {type: String, required: true}, 
+    password: {type: String, required: true, minlength: 8}, 
     userName: {type: String, required: true}, 
     name: {type: String}, 
     img: {type: String}, 
     locations: {type: Number, required: true}
 })
+
+userSchema.plugin(uniqueValidator)
 
 module.exports = mongoose.model('User', userSchema)
