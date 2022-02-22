08dfe501b058f486dcff14ada598e23e5c8f1ed3
rosh <rosherna@Roshernas-MacBook-Air.local>
2022-02-22 19:07:19 +0800

    edited userSchema

--- models/user.js
+++ models/user.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-    name: {type: String}, 
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true}, 
     userName: {type: String, required: true}, 
+    name: {type: String}, 
     img: {type: String}, 
-    email: {type: String, required: true},
-    password: {type: String, required: true}
+    locations: {type: Number, required: true}
 })
 
-module.exports = mongoose.model('User", userSchema')
+module.exports = mongoose.model('User', userSchema)
