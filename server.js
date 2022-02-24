391463d829be5bc1ee246edf10a61db6b7b4fda7
rosh <rosherna@Roshernas-MacBook-Air.local>
2022-02-24 23:29:24 +0800

    adjusted controller sequences

--- server.js
+++ server.js
@@ -23,17 +23,16 @@ app.use((req, res, next) => {
   res.setHeader(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-
-  next();
-});
-
-//Controllers
-const locationController = require("./controllers/locationController");
-const userController = require("./controllers/userController");
-const HttpError = require("./models/error-handler");
-
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    
+    next();
+  });
+  
+  //Controllers
+  const locationController = require("./controllers/locationController");
+  const userController = require("./controllers/userController");
+  const HttpError = require("./models/error-handler");
 
 app.use("/api/locations", locationController);
 app.use("/api/user", userController);
