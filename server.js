b4bf89ae5853d42a0af90d696eb5e3e378ced457
rosh <rosherna@Roshernas-MacBook-Air.local>
2022-02-26 06:34:39 +0800

    remove fs error

--- server.js
+++ server.js
@@ -31,11 +31,7 @@ app.use((req, res) => {
 });
 
 app.use((error, req, res, next) => {
-  if (req.file) {
-    fs.unlink(req.file.path, (err) => {
-      console.log(err);
-    })
-  }
+
   if (res.headerSent) {
     return next(error);
   }
